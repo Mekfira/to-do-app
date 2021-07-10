a6ea594b20b8ac9bc1a4e99b35073742603ccb84
Robel Fekadu <robelfekadu@gmail.com>
2021-07-10 18:47:27 +0300

    Updated 3rd commit

--- app.js
+++ app.js
@@ -38,3 +38,87 @@ if ( favsItems.length === 0 ) {
       }
     }
   });
+
+  lists.addEventListener('click', (event) => {
+
+    const tag = event.target.tagName;
+    const basevalue = event.target.className.baseVal;
+  
+    const clickArea1 = event.target.parentNode.parentNode.parentNode.parentNode.className;
+    const clickArea2 = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.className;
+    const clickArea3 = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.className;
+  
+    // Checking if buttons inside tasks section is clicked=
+    if ( clickArea1 === 'tasks' || clickArea2 === 'tasks' || clickArea3 === 'tasks' ) {
+      if (tag === 'svg') {
+        if (basevalue  === 'delete' || basevalue  === 'can' || basevalue  === 'cap' || basevalue  === 'bin') {
+          let li = event.target.parentNode.parentNode;
+          let ul = li.parentNode;
+          ul.removeChild(li);
+        } else if ( basevalue === 'fav') {
+          let li = event.target.parentNode.parentNode;
+          let ul = li.parentNode;
+          favsSection.style.display = '';
+          favs.appendChild(li);
+          favTitle.textContent = "Favorites";
+        }
+  
+        if ( taskItems.length === 0 ) {
+          tasksSection.style.display = "none";
+        };
+  
+      } else if (tag === 'path') {
+        if (basevalue  === 'delete' || basevalue  === 'can' || basevalue  === 'cap' || basevalue  === 'bin') {
+          let li = event.target.parentNode.parentNode.parentNode.parentNode;
+          let ul = li.parentNode;
+          ul.removeChild(li);
+        } else if ( basevalue === 'favPath') {
+          let li = event.target.parentNode.parentNode.parentNode;
+          let ul = li.parentNode;
+          favsSection.style.display = '';
+          favs.appendChild(li);
+          favTitle.textContent = "Favorites";
+        }
+  
+        if ( taskItems.length === 0 ) {
+          tasksSection.style.display = "none";
+        };
+      }
+    } else if ( clickArea1 === 'favs' || clickArea2 === 'favs' || clickArea3 === 'favs' ) {
+        if (tag === 'svg') {
+          if (basevalue  === 'delete' || basevalue  === 'can' || basevalue  === 'cap' || basevalue  === 'bin') {
+            let li = event.target.parentNode.parentNode;
+            let ul = li.parentNode;
+            ul.removeChild(li);
+          } else if ( basevalue === 'fav') {
+            let li = event.target.parentNode.parentNode;
+            let ul = li.parentNode;
+            tasksSection.style.display = '';
+            tasks.appendChild(li);
+            tasksTitle.textContent = "Inbox";
+          }
+    
+          if ( favsItems.length === 0 ) {
+            favsSection.style.display = 'none';
+          };
+
+        } else if (tag === 'path') {
+            if (basevalue  === 'delete' || basevalue  === 'can' || basevalue  === 'cap' || basevalue  === 'bin') {
+              let li = event.target.parentNode.parentNode.parentNode.parentNode;
+              let ul = li.parentNode;
+              ul.removeChild(li);
+            } else if ( basevalue === 'favPath') {
+              let li = event.target.parentNode.parentNode.parentNode;
+              let ul = li.parentNode;
+              tasksSection.style.display = '';
+              tasks.appendChild(li);
+              tasksTitle.textContent = "Inbox";
+            }
+      
+            if ( favsItems.length === 0 ) {
+              favsSection.style.display = 'none';
+            };
+          }
+        }
+      });
+      
