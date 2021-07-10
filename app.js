0f488290221998ed93ecad78a1e5c2fe64ccd64a
Robel Fekadu <robelfekadu@gmail.com>
2021-07-10 15:36:45 +0300

    This is the 3rd commit with some updates

--- app.js
+++ app.js
@@ -12,3 +12,29 @@ const tasksSelection = document.querySelector('.tasks');
 const tasksTitle = document.querySelector('.tasks h3');
 const tasks = document.querySelector('.tasks ul');
 const taskItems = tasks.children;
+
+if ( favsItems.length === 0 ) {
+    favsSection.style.display = 'none';
+  };
+  
+  if ( taskItems.length === 0 ) {
+    tasksSection.style.display = "none";
+  };
+  
+  taskInput.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+      let li = document.createElement('li');
+      if (taskInput.value === "") {
+        alert("Please add a task");
+      } else {
+        li.textContent = taskInput.value;
+        attachButton(li);
+        tasks.appendChild(li);
+        tasksTitle.style.display = '';
+        taskInput.value = '';
+  
+        tasksTitle.textContent = "Inbox";
+        tasksSection.style.display = "";
+      }
+    }
+  });
