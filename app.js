e68b066fd090d19797d2a03e3b07ed7b773925a5
Robel Fekadu <robelfekadu@gmail.com>
2021-07-10 15:18:17 +0300

    This is the 2nd commit of app.js

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const taskInput = document.querySelector('input.task');
 const lists = document.querySelector('.lists');
 
 const favSection = document.querySelector('.favs');
+const favTitle = document.querySelector('.favs h3');
+const favs = document.querySelector('.favs ul');
+const favsItems = favs.children;
+
+const tasksSelection = document.querySelector('.tasks');
+const tasksTitle = document.querySelector('.tasks h3');
+const tasks = document.querySelector('.tasks ul');
+const taskItems = tasks.children;
